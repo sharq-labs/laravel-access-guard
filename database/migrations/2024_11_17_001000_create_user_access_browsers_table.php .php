ea284ff333ea23711d9e9ac1c4db0b525fae870d
DeveloperKareemElsharkawy <dev.kareem.elsharkawy@gmail.com>
2024-11-17 03:39:03 +0200

    fixing migration bugs

--- database/migrations/2024_11_17_001000_create_user_access_browsers_table.php .php
+++ database/migrations/2024_11_17_001000_create_user_access_browsers_table.php .php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUserAccessBrowsersTableV2   extends Migration
+class CreateUserAccessBrowsersTable   extends Migration
 {
     public function up()
     {
